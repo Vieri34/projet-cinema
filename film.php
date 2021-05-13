186ce126ccc90e6b019bb35e96fc4cf0eb1f1506
Matt <62237111+Vieri34@users.noreply.github.com>
2021-05-13 19:13:58 +0200

    Add files via upload
    révision html/css

--- film.php
+++ film.php
@@ -14,13 +14,14 @@ if(!$film) {
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="CinéDéfi est un site de critiques de films">
-		<link rel="icon" type="image/x-icon" href="images/avataaars.png">
+		<link rel="icon" type="image/x-icon" href="images/new-logo-icon.png">
 		<title>Downtown Film | <?php echo $film['titre']?></title>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous">
+		<link href="css/lightbox.css" rel="stylesheet">
 		<link href="./css/base.css" rel="stylesheet" media="all" type="text/css">
 		<link href="./css/nav.css" rel="stylesheet" media="all" type="text/css">
-		<link href="./css/page-film.css" rel="stylesheet" media="all" type="text/css">
+		<link href="./css/film.css" rel="stylesheet" media="all" type="text/css">
 	</head>
 
 	<body>
@@ -32,44 +33,56 @@ if(!$film) {
 		<main>
 			<!--films-->
 			<section id="page">
-				<a href="#"><img src="<?php echo $film['image']?>" alt=" ..."></a>
+				<a href="<?php echo $film['image']?>" data-lightbox="img"><img src="<?php echo $film['image']?>" alt=" ..."></a>
 				<div class="text-film">
-					<h2 class="titre-main-page"><?php echo $film['titre']?></h2>
-					<div class="staff">
-						<h3 class="realisateur">Réalisateur: </h3>
-						<div class="container-staff">
-							<p><a href="#"><?php echo $film['realisateur']?></a></p>
+
+					<h1 class="titre-main-page"><?php echo $film['titre']?></h1>
+
+					<div id="info-film">
+						<div class="text-lign">
+							<h4 class="realisateur">Film réalisé par:</h4>
+							<a href="#"><?php echo $film['realisateur']?></a>
 						</div>
-						<h3 class="acteur">Acteurs: </h3>
-						<div class="container-staff">
-							<p><a href="#"><?php echo $film['acteurs']?></a></p>
+						<div class="text-lign">
+							<h4 class="acteur">Avec:</h4>
+							<a href="#"><?php echo $film['acteurs']?></a>
+						</div>
+						<div class="text-lign">
+							<h4 class="duree">Durée:</h4>
+							<a href="#"><?php echo $film['duree']?></a>
+						</div>
+						<div class="text-lign">
+							<h4 class="date">Date de sortie:</h4>
+							<a href="#"><?php echo $film['date_sortie']?></a>
 						</div>
 					</div>
-					
-					<h3 class="titre-synopsis">Synopsis</h3>
-					<p class="synopsis"><?php echo $film['synopsis']?></p>
-					<button class="categorie"><?php echo $film['genre']?></button>
+					<div class="synopsis">
+						<h3 class="titre-synopsis">Synopsis</h3>
+						<p class="text-synopsis"><?php echo $film['synopsis']?></p>
+						<a href="#" class="categorie-film custom-btn"><?php echo $film['genre']?></a>
+					</div>
+
 				</div>
 			</section>
 
 			<section id="comments">
 				<h2>Commentaires</h2>
-				<div class="notes">
-					<span class="starRating">
-						<input id="rating5" type="radio" name="rating" value="5">
-						<label for="rating5">5</label>
-						<input id="rating4" type="radio" name="rating" value="4">
-						<label for="rating4">4</label>
-						<input id="rating3" type="radio" name="rating" value="3" checked>
-						<label for="rating3">3</label>
-						<input id="rating2" type="radio" name="rating" value="2">
-						<label for="rating2">2</label>
-						<input id="rating1" type="radio" name="rating" value="1">
-						<label for="rating1">1</label>
-					</span>
-
+				<!--rating system
+				<div class="star-rating">
+					<span class="fa divya fa-star-o" data-rating="1" style="font-size:20px;"></span>
+					<span class="fa fa-star-o" data-rating="2" style="font-size:20px;"></span>
+					<span class="fa fa-star-o" data-rating="3" style="font-size:20px;"></span>
+					<span class="fa fa-star-o" data-rating="4" style="font-size:20px;"></span>
+					<span class="fa fa-star-o" data-rating="5" style="font-size:20px;"></span>
+					<input type="hidden" name="whatever3" class="rating-value" value="1">
 				</div>
-
+				-->
+				<!--commentaires-->
+				<div id="com-inner">
+					<input type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
+					<textarea rows="5" placeholder="Laissez un commentaire ici..." name="comment" id="comment" required></textarea>
+					<button class="custom-btn">Envoyer</button>
+				</div>
 			</section>
 		</main>
 		<!--footer-->
@@ -79,6 +92,12 @@ if(!$film) {
 		<!--scripts-->
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src="js/lightbox.js"></script>
+		<script>
+			lightbox.option({
+			  'fitImagesInViewport': false,
+			})
+		</script>
 		<script src="js/script.js"></script>
 	</body>
 </html>
--- films.php
+++ films.php
@@ -10,7 +10,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="CinéDéfi est un site de critiques de films">
-		<link rel="icon" type="image/x-icon" href="images/logo-V4-crop.png">
+		<link rel="icon" type="image/x-icon" href="images/new-logo-icon.png">
 		<title>Downtown Film | Films sur DownTown Film</title>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous">
